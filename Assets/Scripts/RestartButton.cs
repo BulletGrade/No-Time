c862d3e24321457029833af0c2cdd8227eaaa946
BulletGrade <you@example.com>
2020-06-01 22:40:55 -0300

    Restart Button

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Minus))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
